28c7956225b170e90e772033fb651e973bfb61c4
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-03 15:51:13 +0200

    add AppJumbotron component and relative content inside AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,14 @@
+<script>
+import AppJumbotron from './AppJumbotron.vue';
+
+export default {
+    name: 'AppMain',
+    components: { AppJumbotron }
+}
+</script>
+
+<template>
+    <AppJumbotron />
+</template>
+
+<style lang="scss" scoped></style>
